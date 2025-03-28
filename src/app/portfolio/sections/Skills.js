e9b465403db8a4221b8809dfbc6116c2044bd880
Naveen-Kushalappa <tknaveen4@gmail.com>
2025-03-28 10:31:52 +0530

    Add skills.js

---
+++ src/app/portfolio/sections/Skills.js
@@ -0,0 +1,16 @@
+const Skills = () => {
+  return  <div className="mt-10 md:w-4/5 mx-auto">
+  <div className="border-b-2 border-gray-800 mb-2">
+    <h3 className="font-bold uppercase my-0 tracking-widest">Skills</h3>
+    <div className="border-dancer delay-1000"></div>
+  </div>
+  <p className="text-sm text-gray-1000 font-normal my-4">
+    My skills include but not limited to - JavaScript, HTML, CSS, UI/UX,
+    Mongodb, Express, NextJS, SASS, Python, React, Vue, AngularJS,
+    CI/CD, Configuring Deployment Pipelines, Serverless, AWS, S3,
+    Nodejs, Testing, Code automation, Usage Analysis, Product Development,.etc
+  </p>
+</div>
+};
+
+export default Skills;
